8252096f303907db0a894463e8ebfcf1d8ea53f7
zhepama <250994229@qq.com>
2026-02-06 18:45:13 +0800

    删除不必要的npm包

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from '@tailwindcss/vite'
 import path, { resolve } from 'node:path'
-import checker from 'vite-plugin-checker' // 引入插件
 
 const host = process.env.TAURI_DEV_HOST;
 
@@ -11,12 +10,7 @@ export default defineConfig(async () => ({
   plugins: [
     vue(),
     tailwindcss(),
-    checker({
-      typescript: true,   // 开启 TS 检查
-      vueTsc: true,       // 如果是 vue-tsc 也可以开启
-      // overlay: false,  // 如果你想关闭浏览器红色遮罩层，设为 false；默认是 true
-      terminal: true   // 是否在终端显示错误，默认 true
-    })],
+  ],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './'),
